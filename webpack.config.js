0f91b558b6b9fb68e6eb00c8196e25dae4e2fce5
Vaxied <psgmarcelo@gmail.com>
2024-03-27 19:16:32 -0400

    Adding initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/** @type {import('webpack').Configuration} */
+
+// Importing the mode path from node
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+}
