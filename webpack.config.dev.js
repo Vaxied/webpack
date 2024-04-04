e222df937a0690cff50b7e605ec2cb782fa5480f
Vaxied <psgmarcelo@gmail.com>
2024-04-03 23:05:18 -0400

    Added webpack-bundle-analyzer to view project structure

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,6 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // const CopyPlugin = require('copy-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 
 module.exports = {
     entry: './src/index.js',
@@ -95,5 +96,6 @@ module.exports = {
         //     ],
         // }),
         new Dotenv(),
+        new BundleAnalyzerPlugin(),
     ],
 }
